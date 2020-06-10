ea1ceb58ca0a79e0bd6ee155a280b8ddddcb5138
vinaysaddanapu <61531504+vinaysaddanapu@users.noreply.github.com>
2020-06-10 19:55:19 +0530

    Final Code

--- Announcements/announcements/models/tags.js
+++ Announcements/announcements/models/tags.js
@@ -1,1 +1,13 @@
+const mongoose = require('mongoose');
+const Schema  = mongoose.Schema;
+const ObjectId = mongoose.Schema.Types.ObjectId;
 
+var tags = new Schema({
+
+      employees:[{type:ObjectId, ref:'User'}],
+      interns: [{type:ObjectId, ref:'User'}],
+      techLead:[{type:ObjectId, ref:'User'}]
+
+});
+
+module.exports = mongoose.model("tags", tags);
